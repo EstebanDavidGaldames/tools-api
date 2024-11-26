e3580d04bd8315fda75d895cc365727b44ab0278
Esteban David Galdames <esteban.david.galdames@gmail.com>
2024-11-25 22:54:48 -0300

    Basic CRUD routes created

--- server/api/v1/api_routes.py
+++ server/api/v1/api_routes.py
@@ -3,6 +3,65 @@ from fastapi import APIRouter
 
 router = APIRouter(prefix='/tools')
 
-@router.get('')
-async def get_all():
+
+@router.get(
+    '',
+    status_code=200,
+    responses={
+        200: {'description':'Inventario de herramientas.'},
+    },
+    description= 'Retorna el listado de herramientas disponibles. Si no ha sido inicializado el inventario retorna una lista vacía.',
+)
+async def get_list() -> list:
     return []
+
+
+@router.post(
+    '',
+    status_code=201,
+    responses={
+        201: {'description':'Herramienta inventariada.'},
+    },
+    description= 'Ingresa una herramienta al inventario con los campos pasados por Body Param. Falla si falta alguno de los campos obligatorios.',
+)
+async def create() -> dict:
+    return {}
+
+
+@router.get(
+    '/{id}',
+    status_code=200,
+    responses={
+        200: {'description':'Herramienta encontrada.'},
+        422: {'description':'El ID ingresado no es un número entero válido.'},
+    },
+    description= 'Retorna una herramienta por ID. Falla si el ID no existe.',
+)
+async def get_by_id(id:int) -> dict:
+    return {'id':id}
+
+
+@router.patch(
+    '/{id}',
+    status_code=200,
+    responses={
+        200: {'description':'Herramienta actualizada.'},
+        422: {'description':'El ID ingresado no es un número entero válido.'},
+    },
+    description= 'Actualiza el estado de la herramienta con los datos del Body Param. Falla si el ID no existe.',
+)
+async def update(id:int) -> dict:
+    return {'id':id}
+
+
+@router.delete(
+    '/{id}',
+    status_code=204,
+    responses= {
+        204: {'description': 'Herramienta eliminada del inventario.'},
+        422: {'description':'El ID ingresado no es un número entero válido.'},
+    },
+    description= 'Elimina una herramienta del inventario con ID pasado por Path Param. Falla si el ID no existe.',
+)
+async def delete(id:int) -> None:
+    return None
