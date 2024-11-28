04b3220a7a179e632d7397e04e33339a2a5fcd7e
Esteban David Galdames <esteban.david.galdames@gmail.com>
2024-11-28 07:49:34 -0300

    Logging implemented in controller

--- server/app.py
+++ server/app.py
@@ -1,25 +1,25 @@
-import logging
+#import logging
 
 from fastapi import FastAPI
 
 from .api import api_router
 
 
-logger = logging.getLogger(__name__)
+#logger = logging.getLogger(__name__)
 app = FastAPI()
 
 app.include_router(api_router)
 
 
-@app.on_event('startup')
-async def startup_event():
-    print('\033[92m', 'API Iniciada', '\033[0m')
-    logger.debug('Mensaje de debug.')
-    logger.info('Mensaje de info.')
-    logger.warning('Mensaje de warning.')
-    logger.error('Mensaje de error.')
-    logger.critical('Mensaje de critical.')
+#@app.on_event('startup')
+#async def startup_event():
+#    print('\033[92m', 'API Iniciada', '\033[0m')
+#    logger.debug('Mensaje de debug.')
+#    logger.info('Mensaje de info.')
+#    logger.warning('Mensaje de warning.')
+#    logger.error('Mensaje de error.')
+#    logger.critical('Mensaje de critical.')
 
-@app.on_event('shutdown')
-def shutdown_event():
-    print('\033[91m', 'API Finalizada', '\033[0m')
+#@app.on_event('shutdown')
+#def shutdown_event():
+#    print('\033[91m', 'API Finalizada', '\033[0m')
--- server/controller/tools_api_controller.py
+++ server/controller/tools_api_controller.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List
 
 # from fastapi import HTTPException # No se utiliza - Eliminar import
@@ -7,21 +8,24 @@ from server.exceptions import BaseHTTPException, InternalServerError, NotFound
 from server.service import ToolsService
 
 
+logger = logging.getLogger(__name__)
+
+
 class ToolsController:
     def __init__(self):
         self.service = ToolsService()
 
     def create(self, new_tool: NewToolRequest) -> ToolResponse:
         try:
+            logger.debug(f'Ingreso de nueva herramienta: {new_tool.tool_name}.')
             # Llamada a la capa de servicio para que realice la acción correspondiente
             # Retorna data de ejemplo
             #return ToolResponse(id= 1, **new_tool.model_dump())
             return self.service.create(new_tool)
         except BaseHTTPException as ex:
-            # Se debe implementar logging
-            raise ex
+            self.__handler_http_exception(ex)
         except Exception:
-            # Error no implementado en ToolsController.create
+            logger.critical(f'Error no contemplado en el controlador {__name__}.create')
             raise InternalServerError()
 
     def get_list(self, limit: int, offset: int) -> List[ToolResponse]:
@@ -31,23 +35,22 @@ class ToolsController:
             #return []
             return self.service.get_list(limit, offset)
         except BaseHTTPException as ex:
-            # Se debe implementar loggin
-            raise ex
+            self.__handler_http_exception(ex)
         except Exception:
-            # Error no implementado en ToolsController.create
+            logger.critical(f'Error no contemplado en el controlador {__name__}.get_list')
             raise InternalServerError()
 
     def get_by_id(self, id: int) -> ToolResponse:
         try:
+            logger.debug(f'Consulta de herramienta - ID: {id}.')
             # Llamada a la capa de servicio para que realice la acción correspondiente
             # Retorna ejemplo de error
             #return NotFound(f'No se encontró la herramienta con id = {id}.')
             return self.service.get_by_id(id)
         except BaseHTTPException as ex:
-            # Se debe implementar loggin
-            raise ex
+            self.__handler_http_exception(ex)
         except Exception:
-            # Error no implementado en ToolsController.create
+            logger.critical(f'Error no contemplado en el controlador {__name__}.get_by_id')
             raise InternalServerError()
     
     def update(self, id: int, new_tool_data: ToolRequest) -> ToolResponse:
@@ -57,10 +60,9 @@ class ToolsController:
             #return ToolResponse(id=id, **new_tool_data.model_dump())
             return self.service.update(id, new_tool_data)
         except BaseHTTPException as ex:
-            # Se debe implementar loggin
-            raise ex
+            self.__handler_http_exception(ex)
         except Exception:
-            # Error no implementado en ToolsController.create
+            logger.critical(f'Error no contemplado en el controlador {__name__}.update')
             raise InternalServerError()
 
     def delete(self, id: int) -> None:
@@ -70,8 +72,14 @@ class ToolsController:
             #return
             self.service.delete(id)
         except BaseHTTPException as ex:
-            # Se debe implementar loggin
-            raise ex
+            self.__handler_http_exception(ex)
         except Exception:
-            # Error no implementado en ToolsController.create
+            logger.critical(f'Error no contemplado en el controlador {__name__}.delete')
             raise InternalServerError()
+
+    def __handler_http_exception(self, ex: BaseHTTPException):
+        if ex.status_code >= 500:
+            logger.critical(f'Error en el servidor - STATUS CODE {ex.status_code} : {ex.description}')
+        else:
+            logger.error(f'Error en la solicitud - STATUS CODE {ex.status_code} : {ex.description}')
+        raise ex
